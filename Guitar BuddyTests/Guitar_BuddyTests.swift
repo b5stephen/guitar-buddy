f37468fa3683e9b1899b0c6eb5e12ba8bdb50cb9
Stephen Denekamp <stephen@etched.dev>
2026-09-05 18:42:34 +1200

    Initial Commit

---
+++ Guitar BuddyTests/Guitar_BuddyTests.swift
@@ -0,0 +1,17 @@
+//
+//  Guitar_BuddyTests.swift
+//  Guitar BuddyTests
+//
+//  Created by Stephen Denekamp on 05/09/2026.
+//
+
+import Testing
+@testable import Guitar_Buddy
+
+struct Guitar_BuddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
